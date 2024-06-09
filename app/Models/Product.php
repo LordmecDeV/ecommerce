66295251fd58ff0bdf20d26ffe7398b208edd1c6
Johnny Almeida <johnny.de.felisbino@gmail.com>
2024-06-09 01:00:17 -0300

    wip

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,7 +12,7 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name', 'description', 'price', 'carrousel', 'image_product_1', 'image_product_2', 'image_product_3', 'image_product_4', 'image_product_5', 'image_product_6', 'image_product_7', 'image_product_8', 'image_product_9', 'image_product_10', 'tag', 'sku', 'type_product', 'status',
+        'name', 'description', 'price', 'carrousel', 'image_product_1', 'image_product_2', 'image_product_3', 'image_product_4', 'image_product_5', 'image_product_6', 'image_product_7', 'image_product_8', 'image_product_9', 'image_product_10', 'tag', 'sku', 'type_product', 'status', 'from_price',
     ];
 
     public function carts()
